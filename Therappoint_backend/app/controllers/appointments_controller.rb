6941abc5a5292a036a91b67b4e3b534cded3125f
GAierken <g.aierken@gmail.com>
2020-02-05 21:50:46 -0500

    appointment destroy and update added

--- Therappoint_backend/app/controllers/appointments_controller.rb
+++ Therappoint_backend/app/controllers/appointments_controller.rb
@@ -15,8 +15,25 @@ class AppointmentsController < ApplicationController
  
      end 
 
+     def update
+        appointment = Appointment.find(params[:id])
+        appointment.update(update_params)
+        render json: appointment
+     end 
+
+     def destroy
+         appointment = Appointment.find(params[:id])
+         appointment.destroy
+         render json: appointment
+
+     end 
+
      private
      def appointment_params
          params.permit(:client_id, :provider_id, :appoint_date)
+     end 
+
+     def update_params
+        params.permit(:appoint_date)
      end 
 end
